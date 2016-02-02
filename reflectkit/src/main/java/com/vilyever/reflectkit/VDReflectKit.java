add29c2074d8997aa3893313fbf94c1f61327b93
Deng <ws0250450035@gmail.com>
2016-02-02 11:27:40 +0800

    update comment

--- reflectkit/src/main/java/com/vilyever/reflectkit/VDReflectKit.java
+++ reflectkit/src/main/java/com/vilyever/reflectkit/VDReflectKit.java
@@ -13,36 +13,28 @@ import java.util.ArrayList;
 public class VDReflectKit {
     private final VDReflectKit self = this;
 
-
-    /* #Constructors */
-
-    /* #Overrides */    
-    
-    /* #Accessors */     
-     
-    /* #Delegates */     
-     
-    /* #Private Methods */    
-    
-    /* #Public Methods */
+    /* Public Methods */
+    /** @see #getFields(Class, FieldsExclusionDelegate) */
     public static ArrayList<Field> getFields(Class<?> clazz) {
         return getFields(clazz, clazz, null);
     }
 
+    /** @see #getFields(Class, FieldsExclusionDelegate) */
     public static ArrayList<Field> getFields(Class<?> clazz, Class<?> ancestorClazz) {
         return getFields(clazz, ancestorClazz, null);
     }
 
+    /** @see #getFields(Class, FieldsExclusionDelegate) */
     public static ArrayList<Field> getFields(Class<?> clazz, FieldsExclusionDelegate exclusionDelegate) {
         return getFields(clazz, clazz, exclusionDelegate);
     }
 
     /**
-     *
-     * @param clazz 当前类
-     * @param ancestorClazz 追溯到父类
-     * @param exclusionDelegate 排除的属性字段
-     * @return 属性字段集合
+     * 获取指定class追溯到其某一级父class排除条件以外的所有变量field
+     * @param clazz 指定class
+     * @param ancestorClazz 追溯某一级父class
+     * @param exclusionDelegate 排除条件回调
+     * @return field集合
      */
     public static ArrayList<Field> getFields(Class<?> clazz, Class<?> ancestorClazz, FieldsExclusionDelegate exclusionDelegate) {
         ArrayList<Field> fieldList = new ArrayList<Field>();
@@ -67,24 +59,27 @@ public class VDReflectKit {
         return fieldList;
     }
 
+    /** @see #getMethods(Class, MethodsExclusionDelegate) */
     public static ArrayList<Method> getMethods(Class<?> clazz) {
         return getMethods(clazz, clazz, null);
     }
 
+    /** @see #getMethods(Class, MethodsExclusionDelegate) */
     public static ArrayList<Method> getMethods(Class<?> clazz, Class<?> ancestorClazz) {
         return getMethods(clazz, ancestorClazz, null);
     }
 
+    /** @see #getMethods(Class, MethodsExclusionDelegate) */
     public static ArrayList<Method> getMethods(Class<?> clazz, MethodsExclusionDelegate exclusionDelegate) {
         return getMethods(clazz, clazz, exclusionDelegate);
     }
 
     /**
-     *
-     * @param clazz 当前类
-     * @param ancestorClazz 追溯到父类
-     * @param exclusionDelegate 排除的属性字段
-     * @return 属性字段集合
+     * 获取指定class追溯到其某一级父class排除条件以外的所有方法method
+     * @param clazz 指定class
+     * @param ancestorClazz 追溯某一级父class
+     * @param exclusionDelegate 排除条件回调
+     * @return method集合
      */
     public static ArrayList<Method> getMethods(Class<?> clazz, Class<?> ancestorClazz, MethodsExclusionDelegate exclusionDelegate) {
         ArrayList<Method> methodList = new ArrayList<Method>();
@@ -109,9 +104,7 @@ public class VDReflectKit {
         return methodList;
     }
 
-    /* #Classes */
-
-    /* #Interfaces */
+    /* Interfaces */
     public interface FieldsExclusionDelegate {
         boolean shouldExclude(Field field);
     }
@@ -119,8 +112,4 @@ public class VDReflectKit {
     public interface MethodsExclusionDelegate {
         boolean shouldExclude(Method method);
     }
-
-    /* #Annotations @interface */    
-    
-    /* #Enums */
 }
